ef86466ed3a17c88daa94e196bf698f0844252d4
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-05-26 23:56:22 -0400

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -1,34 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
-import time
-import os
-
-# --- Setup virtual display (headless) ---
-display = Display(visible=0, size=(1200, 1200))
+display = Display(visible=0, size=(800, 800))  
 display.start()
 
-# --- Auto install chromedriver ---
-chromedriver_autoinstaller.install()
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
 
-# --- Setup Chrome options ---
-chrome_options = Options()
+chrome_options = Options()  
+# Add your options as needed    
 options = [
-    "--window-size=1200,1200",
-    "--ignore-certificate-errors",
-    "--headless",
-    "--disable-gpu",
-    "--no-sandbox",
-    "--disable-dev-shm-usage",
-    '--remote-debugging-port=9222'
+  # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
+    #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
+    #'--remote-debugging-port=9222'
 ]
+
 for option in options:
     chrome_options.add_argument(option)
 
-driver = webdriver.Chrome(options=chrome_options)
+    
+driver = webdriver.Chrome(options = chrome_options)
 
 driver.get('http://github.com')
 print(driver.title)
