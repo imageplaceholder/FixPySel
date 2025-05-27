40a6b21d47f23e8431ffacc02179dc5fbee9f25d
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-05-27 00:54:52 -0400

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
 display = Display(visible=0, size=(800, 800))  
@@ -11,7 +10,7 @@ chromedriver_autoinstaller.install()  # Check if the current version of chromedr
                                       # and if it doesn't exist, download it automatically,
                                       # then add chromedriver to path
 
-chrome_options = Options()  
+chrome_options = webdriver.ChromeOptions()
 # Add your options as needed
 options = [
    # Define window size here
