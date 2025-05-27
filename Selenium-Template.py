fec0e146c1e7e133827f4133478e658f9265e350
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-05-27 00:13:02 -0400

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -13,11 +13,19 @@ chromedriver_autoinstaller.install()  # Check if the current version of chromedr
                                       # then add chromedriver to path
 
 chrome_options = Options()  
-# Add your options as needed    
+# Add your options as needed
 options = [
-    "--window-size=1200,1200",
-    "--ignore-certificate-errors",
+   # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
     #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
     "--disable-gpu",
     "--no-sandbox",
     "--disable-dev-shm-usage",
